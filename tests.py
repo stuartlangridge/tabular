c19d3e074cbdd62e4ea0adf69305dcd89fb3f68e
Stuart Langridge <sil@kryogenix.org>
2020-05-23 04:32:39 +0100

    remove commented link

--- tests.py
+++ tests.py
@@ -8,7 +8,6 @@ class TestParser(unittest.TestCase):
         self.maxDiff = None
         lines = inp.split("\n")
         # first line is blank because of formatting
-        #res = tabular.parse_lines(lines[1 + extraskip:])
         res = tabular.parse(filename=None, data_from_tests=lines[1 + extraskip:])
         res_as_dict = [dict(x) for x in res]
         return self.assertEqual(res_as_dict, exp)
